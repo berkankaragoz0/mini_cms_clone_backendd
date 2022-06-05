079be36ffbaa9ac63ab16e0354f1d5ef8dda7b7c
Berkan Karagöz <berkan@saatteknoloji.com>
2022-06-05 11:50:58 +0300

    my team leader is advice 2

--- src/main/java/com/example/mini_cms_clone_backend/controller/ContentController.java
+++ src/main/java/com/example/mini_cms_clone_backend/controller/ContentController.java
@@ -2,6 +2,7 @@ package com.example.mini_cms_clone_backend.controller;
 
 import com.example.mini_cms_clone_backend.pojo.ContentPojo;
 import com.example.mini_cms_clone_backend.service.implementation.ContentServices;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,19 +13,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/contents")
+@AllArgsConstructor
 public class ContentController{
 
     private final ContentServices contentServices;
-
-    public ContentController(ContentServices contentServices) {
-        this.contentServices = contentServices;
-    }
 
     @RequestMapping(value = "/getall", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<ContentPojo>> allContentList() {
         return ResponseEntity.ok(contentServices.getAllContents());
     }
-
-
 }
